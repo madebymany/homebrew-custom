99b2810c785d111b71e2cd7faf50b7c829351568
Dan Brown <dan@madebymany.co.uk>
2013-09-16 17:13:27 +0100

    Clarified version in pass

--- pass.rb
+++ pass.rb
@@ -4,6 +4,7 @@ class Pass < Formula
   homepage 'https://github.com/madebymany/password-store'
   url 'https://github.com/madebymany/password-store/archive/v1.0-pre2.tar.gz'
   sha256 'ad7af6e0b6b2a1816be9234769de00000fdb522044af4513a8c598a3bb6d0ce5'
+  version '1.0-pre2'
 
   head 'https://github.com/madebymany/password-store.git'
 

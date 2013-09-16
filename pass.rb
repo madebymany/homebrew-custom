5cd98af1ce144aa22a6f6faf42fc8b5f1b91789d
Dan Brown <dan@madebymany.co.uk>
2013-09-16 17:28:23 +0100

    pass up to 2.0.0-pre1 now

--- pass.rb
+++ pass.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Pass < Formula
   homepage 'https://github.com/madebymany/password-store'
-  url 'https://github.com/madebymany/password-store/archive/v1.0-pre2.tar.gz'
-  sha256 'ad7af6e0b6b2a1816be9234769de00000fdb522044af4513a8c598a3bb6d0ce5'
-  version '1.0-pre2'
+  url 'https://github.com/madebymany/password-store/archive/v2.0.0-pre1.tar.gz'
+  sha256 '7dd04061204158ca03f4af73ba93f5600cf40769eba4972079edc8e699a8fbec'
+  version '2.0.0-pre1'
 
   head 'https://github.com/madebymany/password-store.git'
 

098b3f22b47c5fd47eb3ec83b5617407ac63bd57
Alex Barlow <alexbarlowis@gmail.com>
2013-10-15 10:31:53 +0100

    Update appalling

--- appalling.rb
+++ appalling.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Appalling < Formula
   homepage 'https://github.com/madebymany/appalling-cmd'
-  version '0.1'
-  url 'https://github.com/madebymany/appalling-cmd/archive/v0.1.tar.gz'
-  sha1 '89699b41f32f45556046c10068c37a4695205ad1'
+  version '0.2'
+  url "https://github.com/madebymany/appalling-cmd/archive/#{version}.tar.gz"
+  sha1 '33eea6abd0e241b796967cdbd2aab9bc44db45c9'
 
   head 'git@github.com:madebymany/appalling-cmd.git'
 

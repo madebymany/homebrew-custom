bdbc7eb3857a4d95cb27598a36ca9c1bc4ec38ee
Dan Brown <dan@madebymany.co.uk>
2014-08-13 14:12:15 +0100

    Update moltar to 1407935451

--- moltar.rb
+++ moltar.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Moltar < Formula
   homepage 'https://github.com/madebymany/moltar'
-  version '1407248053'
-  sha256 '1ed0b0218667be24569761d7e31baa2a0fb16075c42e8bd8e08c2d36921160b0'
+  version '1407935451'
+  sha256 'cccbc1a9390d8dca8eb1d980a34ef3493b0055e11a4d862d6d52ace07d7bd686'
   url "https://mxm-golang-binaries.s3.amazonaws.com/moltar/moltar-#{version}.gz"
 
   head 'https://github.com/madebymany/moltar.git'

1956c2ae31dc9773f0c9706ecb053edd68bacbb1
Ryan McGrath <r.w.mcgrath@gmail.com>
2014-10-08 15:46:31 +0100

    Updated SHA hash

--- moltar.rb
+++ moltar.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Moltar < Formula
   homepage 'https://github.com/madebymany/moltar'
-  version '1412073904'
-  sha256 'cdbb427a43013ffa0956ba23f0184b3bae7d370153d9b6b1ea37d33e384531c3'
+  version '1412073904
+  sha256 'c603dbe9079805ffdce7f469195be330a9fd5b66ecd4fc21bd1b1283117f1ba4'
   url "https://mxm-golang-binaries.s3.amazonaws.com/moltar/moltar-#{version}.gz"
 
   head 'https://github.com/madebymany/moltar.git'

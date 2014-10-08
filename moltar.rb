f9b53b3e86e581e51d154862871a5c24fc2c2d8f
Ryan McGrath <r.w.mcgrath@gmail.com>
2014-10-08 15:46:46 +0100

    added missing '

--- moltar.rb
+++ moltar.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Moltar < Formula
   homepage 'https://github.com/madebymany/moltar'
-  version '1412073904
+  version '1412073904'
   sha256 'c603dbe9079805ffdce7f469195be330a9fd5b66ecd4fc21bd1b1283117f1ba4'
   url "https://mxm-golang-binaries.s3.amazonaws.com/moltar/moltar-#{version}.gz"
 

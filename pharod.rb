d61c3c5ec90f14e6818b714ae70e5db156c3f645
Dan Brown <dan@madebymany.co.uk>
2015-03-23 16:06:08 +0000

    Updated pharod to 1427115488

--- pharod.rb
+++ pharod.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Pharod < Formula
   homepage 'https://github.com/madebymany/pharod'
-  version '1426174398'
-  sha256 '7067ac4c7d376acf943274fc9e3ac1f72603477bb6968e360cea36c780c5801f'
+  version '1427115488'
+  sha256 '65c9be67e706d25247ba983219032100b31757221914f6c4c72853ac0ded5295'
   url "https://mxm-golang-binaries.s3.amazonaws.com/pharod/pharod-#{version}.tar.gz"
 
   head 'https://github.com/madebymany/moltar.git'

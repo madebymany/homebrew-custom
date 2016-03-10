07f2ec35ceee3e2c631b82f1d3c9b68164c0adfc
Zoltrain <ryan@madebymany.co.uk>
2016-03-10 14:44:21 +0000

    Bumped new release

--- kmsenv.rb
+++ kmsenv.rb
@@ -5,9 +5,9 @@
 class Kmsenv < Formula
   desc "Manage environment variables using AWS KMS and S3"
   homepage "https://github.com/madebymany/kmsenv"
-  url "https://github.com/madebymany/kmsenv/archive/v0.0.4.tar.gz"
-  version "0.0.4"
-  sha256 "3d5b1e8d545a534ac8e927823cb93f6843125806066dc6476ead00dd794dbe9b"
+  url "https://github.com/madebymany/kmsenv/archive/v0.0.5.tar.gz"
+  version "0.0.5"
+  sha256 "8122fba2b51a1a173805d8c2b33202a8e97ebeb5f9a6d44ff3afb8ae1118da65"
 
   depends_on "awscli"
 

65e57d1b1274403c703743afa65b6d03eefe08a2
Zoltrain <ryan@madebymany.co.uk>
2016-08-09 17:24:15 +0100

    Fixed url error with biscuit

--- biscuit.rb
+++ biscuit.rb
@@ -5,7 +5,7 @@
 class Biscuit < Formula
   desc "Manage environment variables using AWS KMS and S3"
   homepage "https://github.com/madebymany/kmsenv"
-  url "https://github.com/dcoker/biscuit/releases/download/v0.1.2/biscuit-darwin_amd64.tar.gz"
+  url "https://github.com/dcoker/biscuit/releases/download/v0.1.2/biscuit-darwin_amd64.tgz"
   version "0.1.2"
   sha256 "34119e91333ae01ada2b936aca4ca2ea6b8cc8eeb28b31fecfecb1b5d080b497"
 

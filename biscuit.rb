796d4d5ab53e75cdd0e279bc8e81f92b11cad33e
Zoltrain <ryan@madebymany.co.uk>
2016-08-09 17:23:04 +0100

    Fixed class name

--- biscuit.rb
+++ biscuit.rb
@@ -2,7 +2,7 @@
 #                http://www.rubydoc.info/github/Homebrew/homebrew/master/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 
-class Kmsenv < Formula
+class Biscuit < Formula
   desc "Manage environment variables using AWS KMS and S3"
   homepage "https://github.com/madebymany/kmsenv"
   url "https://github.com/dcoker/biscuit/releases/download/v0.1.2/biscuit-darwin_amd64.tar.gz"
